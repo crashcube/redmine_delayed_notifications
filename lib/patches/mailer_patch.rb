0c02c72a3cb948aa64dda19b8687f97d7d1f0d36
Denis Lysenko <dlysenko@me.com>
2013-12-11 13:29:40 +0400

    Refactoring

--- lib/patches/mailer_patch.rb
+++ lib/patches/mailer_patch.rb
@@ -25,33 +25,7 @@ module Zeed
         end
       end
 
-      issue = journal.journalized
-
       if to_users.kind_of?(Array)
-        instant = false
-      else
-        instant = true
-        to_users = issue.notified_users
-        cc_users = issue.notified_watchers - to_users
-      end
-
-      redmine_headers 'Project' => issue.project.identifier,
-                      'Issue-Id' => issue.id,
-                      'Issue-Author' => issue.author.login
-      redmine_headers 'Issue-Assignee' => issue.assigned_to.login if issue.assigned_to
-      message_id journal
-      references issue
-      @author = journal.user
-      s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] "
-      s << "(#{issue.status.name}) " if journal.new_value_for('status_id')
-      s << issue.subject
-      @issue = issue
-      @users = to_users + cc_users
-      @journal = journal
-      @journal_details = journal.visible_details(@users.first)
-      @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue, :anchor => "change-#{journal.id}")
-
-      unless instant
         Notification.create(
             :action => 'issue_edit',
             :entity_id => issue.id,
@@ -61,11 +35,14 @@ module Zeed
 
         to_users = []
         cc_users = []
+      else
+        issue = journal.journalized
+        to_users = issue.notified_users
+        cc_users = issue.notified_watchers - to_users
       end
 
-      mail :to => to_users.map(&:mail),
-           :cc => cc_users.map(&:mail),
-           :subject => s
+      issue_edit_without_delay(journal, to_users, cc_users)
+
     end
 
     def wiki_content_updated_with_delay(wiki_content, instant = false)
@@ -73,20 +50,6 @@ module Zeed
       if wiki_content.kind_of?(Array)
         wiki_content = wiki_content.last
       end
-
-      redmine_headers 'Project' => wiki_content.project.identifier,
-                      'Wiki-Page-Id' => wiki_content.page.id
-      @author = wiki_content.author
-      message_id wiki_content
-      recipients = wiki_content.recipients
-      cc = wiki_content.page.wiki.watcher_recipients + wiki_content.page.watcher_recipients - recipients
-      @wiki_content = wiki_content
-      @wiki_content_url = url_for(:controller => 'wiki', :action => 'show',
-                                  :project_id => wiki_content.project,
-                                  :id => wiki_content.page.title)
-      @wiki_diff_url = url_for(:controller => 'wiki', :action => 'diff',
-                               :project_id => wiki_content.project, :id => wiki_content.page.title,
-                               :version => wiki_content.version)
 
       unless instant
         Notification.create(
@@ -96,13 +59,13 @@ module Zeed
             :param_model => wiki_content.class.name
         )
 
-        recipients = []
-        cc = []
+        wiki_content.recipients = []
+        wiki_content.page.wiki.watcher_recipients = []
+        wiki_content.page.watcher_recipients = []
       end
 
-      mail :to => recipients,
-           :cc => cc,
-           :subject => "[#{wiki_content.project.name}] #{l(:mail_subject_wiki_content_updated, :id => wiki_content.page.pretty_title)}"
+      wiki_content_updated_without_delay(wiki_content)
+
     end
 
   end
